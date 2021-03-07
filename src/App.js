d394d587e93268a0110aaa33f34a679ca234cf11
modivishal <modivishal.modi21@gmail.com>
2021-03-06 21:09:56 -0500

    added code for socket io client side

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
 import './App.css';
-import React, { useEffect, useMemo, useState  } from 'react';
+import React, { useEffect, useMemo, useState, useRef  } from 'react';
 import { createEditor } from 'slate';
 import { Slate, Editable, withReact } from 'slate-react';
+import io from "socket.io-client";
+
+const socket = io("http://localhost:4000");
 
 const App = () => {
   const editor = useMemo(() => withReact(createEditor()), [])
-  // Add the initial value when setting up our state.
+  const remote = useRef(false);
+  const id = useRef(`${Date.now()}`);
   const [value, setValue] = useState([
     {
       type: 'paragraph',
-      children: [{ text: 'A line of text in a paragraph.' }],
+      children: [{ text: 'Enter Something here' }],
     },
   ])
+
+  useEffect(() => {
+    socket.on(
+      "new-remote-operations",
+      ({editorId, ops}) => {
+        if (id.current !== editorId) {
+          console.log(ops);
+          remote.current = true;
+          JSON.parse(ops).forEach((op) =>{
+            editor.apply(op);
+           }
+         );
+         remote.current = false;
+        }
+      }
+    );
+  }, [editor]);
+  
 
   return (
     <Slate
       editor={editor}
       value={value}
-      onChange={newValue => setValue(newValue)}
+      onChange={newValue => {
+        setValue(newValue);
+
+        const ops = editor.operations
+        .filter(o => {
+          if (o) {
+            return (
+              o.type !== "set_selection" &&
+              o.type !== "set_value" &&
+              (!o.data || !o.data.hasOwnProperty("source"))
+            );
+          }
+
+          return false;
+        })
+        .map((o) => ({ ...o, data: { source: "one" } }));
+
+        if (ops.length && !remote.current) {
+          socket.emit("new-operations", {
+            editorId: id.current,
+            ops: JSON.stringify(ops)
+          });
+        }
+      }}
     >
       <Editable />
     </Slate>
