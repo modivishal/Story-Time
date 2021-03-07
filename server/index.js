b04374d9a6e0bb1727094535dc66a29075aebb38
modivishal <modivishal.modi21@gmail.com>
2021-03-07 14:48:32 -0500

    Formatted the code

--- server/index.js
+++ server/index.js
@@ -9,73 +9,64 @@ let operations = [];
 const maxUser = 3;
 
 const scheduleUserTurn = () => {
-    console.log("scheduling user",userIds[userTurn]);
-    io.emit("user-turn", userIds[userTurn]);
-}
+  io.emit("user-turn", userIds[userTurn]);
+};
 
 const nextUserTurn = () => {
-    userTurn = (userTurn + 1) % (userIds.length);
-    scheduleUserTurn();
-}
+  userTurn = (userTurn + 1) % userIds.length;
+  scheduleUserTurn();
+};
 
 const startTimer = () => {
-    if (!userTime){
-        userTime = setInterval(nextUserTurn, 10000);
-    }
-}
+  if (!userTime) {
+    userTime = setInterval(nextUserTurn, 10000);
+  }
+};
 
 const getUserIndex = (user) => {
-    console.log("userIds",userIds);
-    for (let index = 0; index < userIds.length; index++) {
-       if (userIds[index]===user){
-           return index;
-       }
+  for (let index = 0; index < userIds.length; index++) {
+    if (userIds[index] === user) {
+      return index;
     }
-}
+  }
+};
 
 io.on("connection", (socket) => {
+  if (userIds.length >= maxUser) {
+    socket.emit("err", "Maximum user limit reached");
+    socket.disconnect();
+    return;
+  }
 
-    if (userIds.length >= maxUser){
-        socket.emit("err","Maximum user limit reached");
-        socket.disconnect();
-        console.log("maxUser limit");
-        return;
-    }
-
-    userIds.push(socket.id);
-    if (userIds.length === 1){
-
-        scheduleUserTurn();
-        startTimer();
-    }
-    if (operations.length>0){
-        console.log("initial-user-id", socket.id);
-        io.emit("initial-operations", {
-            userId: socket.id,
-            ops: JSON.stringify(operations)
-        });
-    }
-      
-    socket.on("disconnect",() => {
-        console.log("sesssssssssion",socket.id);
-        let index = getUserIndex(socket.id);
-        console.log("indexindexindex",index);
-        userIds.splice(index,1);
-        if(userIds.length === 0){
-            operations = [];
-            clearInterval(userTime);
-            userTime = false;
-            userTurn = 0;
-        }
+  userIds.push(socket.id);
+  if (userIds.length === 1) {
+    scheduleUserTurn();
+    startTimer();
+  }
+  if (operations.length > 0) {
+    io.emit("initial-operations", {
+      userId: socket.id,
+      ops: JSON.stringify(operations),
     });
+  }
 
-    socket.on("new-operations", (data) => {
-        io.emit("new-remote-operations", data);
-        operations = operations.concat(JSON.parse(data.ops));
-    });
-  
+  socket.on("disconnect", () => {
+    let index = getUserIndex(socket.id);
+    userIds.splice(index, 1);
+    if (userIds.length === 0) {
+      operations = [];
+      clearInterval(userTime);
+      userTime = false;
+      userTurn = 0;
+    }
+  });
+
+  socket.on("new-operations", (data) => {
+    io.emit("new-remote-operations", data);
+    operations = operations.concat(JSON.parse(data.ops));
+  });
 });
 
-http.listen(4000, function() {
+http.listen(4000, function () {
   console.log("listening on *:4000");
 });
--- src/App.js
+++ src/App.js
@@ -1,121 +1,97 @@
-import './App.css';
-import React, { useEffect, useMemo, useState, useRef  } from 'react';
-import { createEditor } from 'slate';
-import { Slate, Editable, withReact } from 'slate-react';
+import "./App.css";
+import React, { useEffect, useMemo, useState, useRef } from "react";
+import { createEditor } from "slate";
+import { Slate, Editable, withReact } from "slate-react";
 import io from "socket.io-client";
 
 const socket = io("http://localhost:4000");
 
 const App = () => {
-  const editor = useMemo(() => withReact(createEditor()), [])
+  const editor = useMemo(() => withReact(createEditor()), []);
   const remote = useRef(false);
   const id = useRef();
   const [value, setValue] = useState([
     {
-      type: 'paragraph',
-      children: [{ text: 'Enter Something here' }],
+      type: "paragraph",
+      children: [{ text: "Enter Something:" }],
     },
   ]);
   const [readonly, setreadonly] = useState(true);
 
   useEffect(() => {
+    socket.on("connect", () => {
+      id.current = socket.id;
+      console.log("socket", socket.id);
+    });
 
-    socket.on(
-      "connect",
-      () => {
-        id.current = socket.id;
-        console.log("socket",socket.id);
+    socket.on("user-turn", (userTurn) => {
+      console.log(userTurn);
+      if (userTurn === id.current) {
+        console.log("mah life mah turn");
+        setreadonly(false);
+      } else {
+        setreadonly(true);
       }
-    );
+    });
 
-    socket.on(
-      "user-turn",
-      (userTurn) => {
-        console.log(userTurn);
-        if(userTurn===id.current){
-          console.log("mah life mah turn");
-          setreadonly(false);
-        }
-        else{
-          setreadonly(true);
-        }
+    socket.on("new-remote-operations", ({ editorId, ops }) => {
+      if (id.current !== editorId) {
+        remote.current = true;
+        JSON.parse(ops).forEach((op) => {
+          editor.apply(op);
+        });
+        remote.current = false;
       }
-    );
+    });
 
-    socket.on(
-      "new-remote-operations",
-      ({editorId, ops}) => {
-        if (id.current !== editorId) {
-          remote.current = true;
-          JSON.parse(ops).forEach((op) =>{
-            editor.apply(op);
-           }
-         );
-         remote.current = false;
-        }
+    socket.on("initial-operations", ({ userId, ops }) => {
+      if (id.current === userId) {
+        remote.current = true;
+        JSON.parse(ops).forEach((op) => {
+          editor.apply(op);
+        });
+        remote.current = false;
       }
-    );
+    });
 
-    socket.on(
-      "initial-operations",
-      ({userId, ops}) => {
-        if (id.current === userId) {
-          remote.current = true;
-          JSON.parse(ops).forEach((op) =>{
-            editor.apply(op);
-           }
-          );
-         remote.current = false;
-        }
-      }
-    );
-
-    socket.on(
-      "err",
-      (msg) => {
-        alert(msg);
-      }
-    );
-
+    socket.on("err", (msg) => {
+      alert(msg);
+    });
   }, [editor]);
-  
 
   return (
     <Slate
       editor={editor}
       value={value}
-      
-      onChange={newValue => {
+      onChange={(newValue) => {
         setValue(newValue);
 
         const ops = editor.operations
-        .filter(o => {
-          if (o) {
-            return (
-              o.type !== "set_selection" &&
-              o.type !== "set_value" &&
-              (!o.data || !o.data.hasOwnProperty("source"))
-            );
-          }
+          .filter((o) => {
+            if (o) {
+              return (
+                o.type !== "set_selection" &&
+                o.type !== "set_value" &&
+                (!o.data || !o.data.hasOwnProperty("source"))
+              );
+            }
 
-          return false;
-        })
-        .map((o) => ({ ...o, data: { source: "one" } }));
+            return false;
+          })
+          .map((o) => ({ ...o, data: { source: "one" } }));
 
         if (ops.length && !remote.current) {
           socket.emit("new-operations", {
             editorId: id.current,
-            ops: JSON.stringify(ops)
+            ops: JSON.stringify(ops),
           });
         }
       }}
     >
-      { !readonly ? <div>It's your turn.</div>: null }
-      <Editable 
-        readOnly={readonly}
-      />
+      {!readonly ? <div>It's your turn.</div> : null}
+      <Editable readOnly={readonly} />
     </Slate>
-  )
+  );
 };
 
 export default App;
